92bd2a3781e3d98578df69f938aca4b17f183d28
Alexandr Morozov <moralex260@gmail.com>
2017-11-14 23:11:44 +0000

    initial

--- js/index.js
+++ js/index.js
@@ -2,11 +2,11 @@ document.getElementById("mybtn").onclick = function() {
     const myaudio = document.getElementById("myaudio");
     if (myaudio.paused == true) {
         myaudio.play();
-        this.style.backgroundImage = "url(images/color-violin.jpg)";
+        this.style.backgroundImage = "url(/images/color-violin.jpg)";
     }
     else if (myaudio.paused == false) {
         myaudio.pause();
-        this.style.backgroundImage = "url(images/btn.png)";
+        this.style.backgroundImage = "url(/images/btn.png)";
     }
 };
 
