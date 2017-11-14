69f2c5206f926497cf38945ae8f27518b1c6b1b8
Alexandr Morozov <moralex260@gmail.com>
2017-11-14 23:01:18 +0000

    initial

---
+++ js/forms.js
@@ -0,0 +1,29 @@
+$('#page-loaded').html( (new Date).toLocaleTimeString() );
+
+$('form input[type="submit"]').click(sendForm);
+
+function sendForm(e) {
+    const form = $('form')[0];
+    if (!form.checkValidity()) {
+        return;
+    }
+    e.preventDefault();
+    $.ajax({
+        url: "https://formspree.io/moralex260@gmail.com", 
+        method: "POST",
+        data: {
+            name: $('#your-name').val(),
+        youremail$('#email').val(),
+            message: $('#your-message').val()
+            
+        },
+        dataType: "json"
+    })
+    .done(function(){
+        $('form')[0].reset();
+        $('#msg').html('Thank you!');
+    })
+    .fail(function(){
+        $('#msg').html('Sorry, there is an error!');
+    });
+}
