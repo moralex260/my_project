addea250d11d86f98b5a067242b2285a1a166c8a
Alexandr Morozov <moralex260@gmail.com>
2017-11-16 17:54:07 +0000

    initial

--- js/index.js
+++ js/index.js
@@ -9,10 +9,6 @@ document.getElementById("mybtn").onclick = function() {
         this.style.backgroundImage = "url(../images/btn.png)";
     }
 };
-let makeEaseOut;
-function makeEaseOut(timing) {
-    return function(timeFraction) {
-        return 1 - timing(1 - timeFraction);
-    };
-}
+
+
 
